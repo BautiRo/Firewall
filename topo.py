4a5a0b88262f11ab0afc9e729d7603aa1c939380
Juan Bautista Rodriguez <jbrodriguez@fi.uba.ar>
2023-05-15 20:20:12 -0300

    Full repo clone

---
+++ topo.py
@@ -0,0 +1,45 @@
+from mininet.topo import Topo
+
+def int2dpid( dpid ):
+   try:
+      dpid = hex( dpid )[ 2: ]
+      dpid = '0' * ( 16 - len( dpid ) ) + dpid
+      return dpid
+   except IndexError:
+      raise Exception( 'Unable to derive default datapath ID - '
+                       'please either specify a dpid or use a '
+		       'canonical switch name such as s23.' )
+
+class MyTopo ( Topo ) :
+	def __init__ ( self, number_switches ) :
+		
+		# Initialize topology
+		Topo.__init__ ( self )
+
+		number_switches += 1
+		# Create switch
+		s_first = self.addSwitch('switch_1', dpid=int2dpid(1))
+		s_last = self.addSwitch('switch_' + str(number_switches + 1), dpid=int2dpid(number_switches + 1))
+		
+		# Create hosts
+		h1 = self.addHost('host_1')
+		h2 = self.addHost('host_2')
+		h3 = self.addHost('host_3')
+		h4 = self.addHost('host_4')
+
+		# Add switches to topology
+		switches = [s_first]
+		switches += [self.addSwitch('switch_' + str(switch), dpid=int2dpid(switch)) for switch in range(2,number_switches+1)]
+		switches.append(s_last)
+
+		# Add links to switches on a linear topology
+		for iter_num in range(len(switches)-1):
+			self.addLink(switches[iter_num],switches[iter_num+1])
+
+		# Add links from hosts to switches
+		self.addLink( s_first, h1 )
+		self.addLink( s_first, h2 )
+		self.addLink( s_last, h3 )
+		self.addLink( s_last, h4 )
+
+topos = {'linearTopology': MyTopo }
